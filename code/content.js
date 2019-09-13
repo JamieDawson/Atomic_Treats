e2eed463f000fdcb9ba1058634f070ab192b4102
JamieDawson <jamierobertdawson@gmail.com>
2019-09-13 16:50:38 -0700

    added NL and fixed spelling error in description

--- code/content.js
+++ code/content.js
@@ -3,124 +3,149 @@ module.exports = [
     text: "How to make a Vanilla Mug Cake",
     tags: ["vanilla", "random"],
     ingredients: ["1/3 cup almond flour",
-                    "1/4 tablespoons Erythritol",
-                    "1/4 teaspoon baking powder",
-                    "1/8 teaspoon salt",
-                    "2 tablespoons heavy whipping cream",
-                    "2 tablespoons butter",
-                    "1 egg",
-                    "1 1/2 teaspoon vanilla extract",
-                    "chopped strawberries (optional)"],
+      "1/4 tablespoons Erythritol",
+      "1/4 teaspoon baking powder",
+      "1/8 teaspoon salt",
+      "2 tablespoons heavy whipping cream",
+      "2 tablespoons butter",
+      "1 egg",
+      "1 1/2 teaspoon vanilla extract",
+      "chopped strawberries (optional)"],
     recipeSteps: [
-        {key: "Step one", 
-         value: "In a large mug, combine the almond flour, baking powder, and salt."},
-        {key: "Step two", 
-         value: "Stir butter, heavy whipping cream, egg, erythritol, and vanilla"},
-        {key: "Step three", 
-         value: "Microwave 90 seconds and enjoy!"},
+      {
+        key: "Step one",
+        value: "In a large mug, combine the almond flour, baking powder, and salt."
+      },
+      {
+        key: "Step two",
+        value: "Stir butter, heavy whipping cream, egg, erythritol, and vanilla"
+      },
+      {
+        key: "Step three",
+        value: "Microwave 90 seconds and enjoy!"
+      },
     ]
   },
-    {
+  {
     text: "How to make a Red Velvet Cake",
     tags: ["red", "velvet", "random"],
     ingredients: ["3 tablespoons heavy whipping cream",
-                    "1/2 teaspoon white vinegar",
-                    "4 tablespoons of almond flour",
-                    "2 tablespoons Erythritol",
-                    "1 teaspoon cocoa powder",
-                    "1/4 teaspoon baking powder",
-                    "pinch of salt",
-                    "1 1/2 tablespoons coconut oil",
-                    "2 teaspoons of vanilla extract",
-                     "8 drops red food coloring, optional"],
+      "1/2 teaspoon white vinegar",
+      "4 tablespoons of almond flour",
+      "2 tablespoons Erythritol",
+      "1 teaspoon cocoa powder",
+      "1/4 teaspoon baking powder",
+      "pinch of salt",
+      "1 1/2 tablespoons coconut oil",
+      "2 teaspoons of vanilla extract",
+      "8 drops red food coloring, optional"],
     recipeSteps: [
-        {key: "Step one", 
-         value: "Stir heavy whipping cream and vinegar in a large mug. Set aside."},
-        {key: "Step two", 
-         value: "In a small bowl, stir almond flour, erythritol, cocoa powder, baking powder, and salt. Add to the heavy whipping cream mixture and add the coconut oil and red food coloring. Stir until it is smooth."},
-        {key: "Step three", 
-         value: "Cook in microwave on high for 1 minute and enjoy!"},
+      {
+        key: "Step one",
+        value: "Stir heavy whipping cream and vinegar in a large mug. Set aside."
+      },
+      {
+        key: "Step two",
+        value: "In a small bowl, stir almond flour, erythritol, cocoa powder, baking powder, and salt. Add to the heavy whipping cream mixture and add the coconut oil and red food coloring. Stir until it is smooth."
+      },
+      {
+        key: "Step three",
+        value: "Cook in microwave on high for 1 minute and enjoy!"
+      },
     ]
   },
-  
-  
-      {
+  {
     text: "How to make a Chocolate Mug Cake",
     tags: ["chocolate", "random"],
     ingredients: ["3 tablespoons of flour",
-                    "3 tablespoons of sugar",
-                    "3 tablespoons of milk",
-                    "3 tablespoons of chocolate chips",
-                    "3 tablespoons coconut oil2 tablespoons cocoa powder",
-                    "2 tablespoons of chocolate frosting",
-                    "1/4 teaspoons baking powder",
-                    "Chocolate sugar free syrup"
-                 ],
+      "3 tablespoons of sugar",
+      "3 tablespoons of milk",
+      "3 tablespoons of chocolate chips",
+      "3 tablespoons coconut oil2 tablespoons cocoa powder",
+      "2 tablespoons of chocolate frosting",
+      "1/4 teaspoons baking powder",
+      "Chocolate sugar free syrup"
+    ],
     recipeSteps: [
-        {key: "Step one", 
-         value: "Grab a large coffee mug"},
-        {key: "Step two", 
-         value: "Add flour, sugar, cocoa powder, and baking powder into the mug. Mix with a fork. Add milk, coconut oil, and chocolate chips and blend until it is smooth."},
-        {key: "Step three", 
-         value: "Microwave for 90 seconds."},
-              {key: "Step four", 
-         value: "Add chocolate syrup on top."},
+      {
+        key: "Step one",
+        value: "Grab a large coffee mug"
+      },
+      {
+        key: "Step two",
+        value: "Add flour, sugar, cocoa powder, and baking powder into the mug. Mix with a fork. Add milk, coconut oil, and chocolate chips and blend until it is smooth."
+      },
+      {
+        key: "Step three",
+        value: "Microwave for 90 seconds."
+      },
+      {
+        key: "Step four",
+        value: "Add chocolate syrup on top."
+      },
     ]
   },
-  
-  
-  
-        {
+  {
     text: "How to make a Keto Friendly Chocolate Mug Cake",
     tags: ["keto", "random"],
     ingredients: ["3 Tbsp of Vanilla",
-                  "3 Tbsp Almond flour",
-                  "2 Tablespoons Erythritol", 
-                  "1 Tablespoon of melted butter",
-                  "1 Tablespoon of cocoa powder",
-                  "1 egg, beaten",
-                  "1/2 teaspoon baking powder",
-                  "sugar free chocolate syrup"
-                 ],
+      "3 Tbsp Almond flour",
+      "2 Tablespoons Erythritol",
+      "1 Tablespoon of melted butter",
+      "1 Tablespoon of cocoa powder",
+      "1 egg, beaten",
+      "1/2 teaspoon baking powder",
+      "sugar free chocolate syrup"
+    ],
     recipeSteps: [
-        {key: "Step one", 
-         value: "In a coffee mug, melt the butter in the microwave for 10 seconds. Add the coconut flour, erythritol, cocoa powder, baking powder, and egg."},
-        {key: "Step two", 
-         value: "Mix all contents. Microwave for high for 60 seconds."},
-        {key: "Step three", 
-         value: "Microwave for 90 seconds."},
-              {key: "Step four", 
-         value: "Cover cake in sugar free chocolate syrup. "},
+      {
+        key: "Step one",
+        value: "In a coffee mug, melt the butter in the microwave for 10 seconds. Add the coconut flour, erythritol, cocoa powder, baking powder, and egg."
+      },
+      {
+        key: "Step two",
+        value: "Mix all contents. Microwave for high for 60 seconds."
+      },
+      {
+        key: "Step three",
+        value: "Microwave for 90 seconds."
+      },
+      {
+        key: "Step four",
+        value: "Cover cake in sugar free chocolate syrup. "
+      },
     ]
   },
-  
-  
-  
-    
-        {
+  {
     text: "How to make a Chocolate Birthday Mug Cake",
     tags: ["chocolate", "birthday", "random"],
     ingredients: ["3 tablespoons of flour",
-                  "3 tablespoons of sugar",
-                  "3 tablespoons of milk",
-                  "3 tablespoons of chocolate chips",
-                  "3 tablespoons of coconut oil",
-                  "2 tablespoons of cocoa powder",
-                  "2 tablespoons of chocolate frosting",
-                  "1/4 teaspoons of baking powder",
-                  "sprinkles"
-                 ],
+      "3 tablespoons of sugar",
+      "3 tablespoons of milk",
+      "3 tablespoons of chocolate chips",
+      "3 tablespoons of coconut oil",
+      "2 tablespoons of cocoa powder",
+      "2 tablespoons of chocolate frosting",
+      "1/4 teaspoons of baking powder",
+      "sprinkles"
+    ],
     recipeSteps: [
-        {key: "Step one", 
-         value: "Grab a large coffee mug."},
-        {key: "Step two", 
-         value: "Put the flour, sugar, cocoa powder, and baking powder into the mug. Mix with a fork. Add milk, coconut oil, and chocolate chips and blend until it is smooth."},
-        {key: "Step three", 
-         value: "Microwave for 90 seconds."},
-              {key: "Step four", 
-         value: "Add sprinkles and enjoy!"},
+      {
+        key: "Step one",
+        value: "Grab a large coffee mug."
+      },
+      {
+        key: "Step two",
+        value: "Put the flour, sugar, cocoa powder, and baking powder into the mug. Mix with a fork. Add milk, coconut oil, and chocolate chips and blend until it is smooth."
+      },
+      {
+        key: "Step three",
+        value: "Microwave for 90 seconds."
+      },
+      {
+        key: "Step four",
+        value: "Add sprinkles and enjoy!"
+      },
     ]
   },
-  
-  
 ]
